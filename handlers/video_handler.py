c4b30b3ec017fdd4ec2bd2efacedfe9acaba3be0
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-06-23 00:53:01 +0700

    Update video_handler.py

--- handlers/video_handler.py
+++ handlers/video_handler.py
@@ -1,14 +1,16 @@
+# handlers/video_handler.py
+
 from handlers.state_manager import set_state
 
-async def handle_video(client, message):
+async def handle_drlvideo(client, message):
     chat_id = message.chat.id
-    set_state(chat_id, "video")
+    set_state(chat_id, "drlvideo")
 
     await message.reply(
-        "🎞️ Kirimkan link video kamu.\n\n"
+        "📽️ Kirimkan link video direct kamu.\n\n"
         "Contoh:\n"
         "`https://example.com/video.mp4`\n"
-        "`https://example.com/stream.m3u8`\n"
-        "`https://example.com/video.ts`",
+        "`https://example.com/video.ts`\n"
+        "`https://example.com/stream.m3u8`",
         quote=True
     )
