804e2d327fd56db84f98c24476d519caa3c3348e
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-06-22 22:41:44 +0700

    Create state_manager.py

---
+++ handlers/state_manager.py
@@ -0,0 +1,3 @@
+set_state(chat_id, mode)      # Simpan mode download user (misal: "gd")
+get_state(chat_id)            # Ambil mode user saat ini
+clear_state(chat_id)          # Hapus state user setelah selesai
